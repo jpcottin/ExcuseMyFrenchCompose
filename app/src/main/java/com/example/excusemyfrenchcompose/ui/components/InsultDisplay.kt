0b699f9cc388c5d65b24cd789fb0f60ea625c857
JP <jpcottin@gmail.com>
2025-02-08 23:40:48 -0800

    Fixing 1 test

--- app/src/main/java/com/example/excusemyfrenchcompose/ui/components/InsultDisplay.kt
+++ app/src/main/java/com/example/excusemyfrenchcompose/ui/components/InsultDisplay.kt
@@ -20,16 +20,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.excusemyfrenchcompose.ui.viewmodel.InsultViewModel
 import com.example.excusemyfrenchcompose.R
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
